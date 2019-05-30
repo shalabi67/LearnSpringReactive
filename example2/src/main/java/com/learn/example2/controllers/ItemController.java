b07c055f50bfd9dcfa9ab16dfd78a72c1096b84c
mohammad <shalabi67@hotmail.com>
2019-05-30 19:07:39 +0200

    fix unit tests

--- example2/src/main/java/com/learn/example2/controllers/ItemController.java
+++ example2/src/main/java/com/learn/example2/controllers/ItemController.java
@@ -3,7 +3,9 @@ package com.learn.example2.controllers;
 import com.learn.example2.models.Item;
 import com.learn.example2.repositories.ItemRepository;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
@@ -28,8 +30,16 @@ public class ItemController {
     }
 
     @PostMapping(produces = MediaType.APPLICATION_STREAM_JSON_VALUE)
+    @ResponseStatus(HttpStatus.CREATED)
     public Mono<Item> addItem(@RequestBody Item item) {
         item.setId(null);
         return itemRepository.save(item);
+    }
+
+    @GetMapping(value="/{id}", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
+    public Mono<ResponseEntity<Item>> getItem(@PathVariable String id) {
+        return itemRepository.findById(id)
+                .map(item -> new ResponseEntity<>(item, HttpStatus.OK))
+                .defaultIfEmpty(new ResponseEntity<>(HttpStatus.NOT_FOUND));
     }
 }
--- example2/src/test/java/com/learn/example2/controllers/ItemControllerTest.java
+++ example2/src/test/java/com/learn/example2/controllers/ItemControllerTest.java
@@ -1,8 +1,10 @@
 package com.learn.example2.controllers;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.learn.example2.factory.ItemFactory;
 import com.learn.example2.models.Item;
 import com.learn.example2.repositories.ItemRepository;
+import org.assertj.core.api.Assertions;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
@@ -10,7 +12,10 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.reactive.server.WebTestClient;
@@ -18,6 +23,9 @@ import org.springframework.web.reactive.function.BodyInserters;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 import reactor.test.StepVerifier;
+
+import java.io.IOException;
+import java.util.List;
 
 @SpringBootTest
 @RunWith(SpringRunner.class)
@@ -57,6 +65,22 @@ public class ItemControllerTest {
                 .expectNextCount(10 + 3 + 1)
                 .verifyComplete();
     }
+    @Test
+    public void testGetAllItems2() {
+        webTestClient
+                .get().uri(ItemController.ITEMS_URL)
+                .accept(MediaType.TEXT_EVENT_STREAM)
+                .exchange()
+                .expectStatus().isOk()
+                .expectBodyList(Item.class)
+                .consumeWith(response -> {
+                    List<Item> items = response.getResponseBody();
+                    for(Item item : items) {
+                        Assert.assertNotNull(item.getId());
+                    }
+                });
+
+    }
 
     @Test
     public void testAddItem() {
@@ -67,7 +91,7 @@ public class ItemControllerTest {
                 .contentType(MediaType.APPLICATION_JSON)
                 .body(BodyInserters.fromObject(item))
                 .exchange()
-                .expectStatus().isOk()
+                .expectStatus().isCreated()
                 .returnResult(Item.class)
                 .getResponseBody();
 
@@ -76,5 +100,61 @@ public class ItemControllerTest {
                 .assertNext(item1 -> Assert.assertEquals("description aaa", item1.getDescription()))
                 //.expectNextCount(1)
                 .verifyComplete();
+    }
+    @Test
+    public void testAddItem2() {
+        Item item = ItemFactory.create();
+        webTestClient
+                .post().uri(ItemController.ITEMS_URL)
+                .accept(MediaType.APPLICATION_STREAM_JSON)
+                .contentType(MediaType.APPLICATION_JSON)
+                .body(BodyInserters.fromObject(item))
+                .exchange()
+                .expectStatus().isCreated()
+                .expectBody(Item.class)
+                .consumeWith(response -> {
+                    Item item1 =response.getResponseBody();
+                    Assert.assertEquals(item.getDescription(), item1.getDescription());
+                    Assert.assertEquals(item.getPrice(), item1.getPrice(), .001);
+                    Assert.assertNotNull(item1.getId());
+                    Assert.assertNotEquals(item.getId(), item1.getId());
+                });
+    }
+
+    @Test
+    public void testGetExistingItem() {
+        ParameterizedTypeReference<ResponseEntity<Item>> typeRef =
+                new ParameterizedTypeReference<ResponseEntity<Item>>() {};
+        webTestClient
+                .get().uri(ItemController.ITEMS_URL + "/aaa")
+                .accept(MediaType.TEXT_EVENT_STREAM)
+                .exchange()
+                .expectStatus().isOk()
+                .expectBody()
+                .consumeWith(response -> {
+                    String result = new String(response.getResponseBody()).substring(5).trim();
+                    ObjectMapper mapper = new ObjectMapper();
+                    try {
+                        Item item = mapper.readValue(result, Item.class);
+                        Assert.assertEquals("aaa", item.getId());
+                        Assert.assertEquals(99.59, item.getPrice(), .001);
+                    } catch (IOException e) {
+                       Assert.fail();
+                    }
+
+                });
+
+                //.jsonPath("$.id").isEqualTo("aaa")
+                //.jsonPath("$.price").isEqualTo(99.59);
+
+    }
+    @Test
+    public void testGetNotFoundItem() {
+        webTestClient
+                .get().uri(ItemController.ITEMS_URL + "/qwe")
+                .accept(MediaType.TEXT_EVENT_STREAM)
+                .exchange()
+                .expectStatus().isNotFound();
+
     }
 }
